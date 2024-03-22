b1254544334fc83d24803d5ef843648aa51e375c
jnbdz <237008+jnbdz@users.noreply.github.com>
2024-03-21 21:51:29 -0400

    Started planning and wirtting th doc

---
+++ source/navcanada/navcanada.go
@@ -0,0 +1,72 @@
+package navcanada
+
+// https://plan.navcanada.ca/weather/api/alpha/
+// ?site=CYJN
+// &site=CYHU
+// &site=CYUL
+//
+// &alpha=sigmet
+// &alpha=airmet
+// &alpha=notam
+// &alpha=metar
+// &alpha=taf
+// &alpha=pirep
+// &alpha=upperwind
+// &alpha=vfr_route
+//
+//  &image=SURFACE_ANALYSIS
+//  &image=UPPER_ANALYSIS//250HPA
+//  &image=UPPER_ANALYSIS//500HPA/THKNS&image=UPPER_ANALYSIS//500HPA/VORT&image=UPPER_ANALYSIS//700HPA
+//  &image=UPPER_ANALYSIS//850HPA
+
+//  &image=COMPOSITE_RADAR/ECHOTOP/NAT
+//  &image=COMPOSITE_RADAR/LOW_RAIN~CAPPI/NAT
+//  &image=COMPOSITE_RADAR/LOW_SNOW~CAPPI/NAT
+//  &image=COMPOSITE_RADAR/ECHOTOP/PAC
+//  &image=COMPOSITE_RADAR/ECHOTOP/WRN
+//  &image=COMPOSITE_RADAR/ECHOTOP/ONT
+//  &image=COMPOSITE_RADAR/ECHOTOP/QUE
+//  &image=COMPOSITE_RADAR/ECHOTOP/ERN
+//  &image=COMPOSITE_RADAR/LOW_RAIN~CAPPI/PAC
+//  &image=COMPOSITE_RADAR/LOW_RAIN~CAPPI/WRN
+//  &image=COMPOSITE_RADAR/LOW_RAIN~CAPPI/ONT
+//  &image=COMPOSITE_RADAR/LOW_RAIN~CAPPI/QUE
+//  &image=COMPOSITE_RADAR/LOW_RAIN~CAPPI/ERN
+//  &image=COMPOSITE_RADAR/LOW_SNOW~CAPPI/PAC
+//  &image=COMPOSITE_RADAR/LOW_SNOW~CAPPI/WRN
+//  &image=COMPOSITE_RADAR/LOW_SNOW~CAPPI/ONT
+//  &image=COMPOSITE_RADAR/LOW_SNOW~CAPPI/QUE
+//  &image=COMPOSITE_RADAR/LOW_SNOW~CAPPI/ERN
+
+//  &image=RADAR/ECHOTOP
+//  &image=RADAR/CAPPI_RAIN
+//  &image=RADAR/CAPPI_SNOW
+
+//  &image=SATELLITE/IR
+//  &image=SATELLITE/VIS
+//  &image=SATELLITE/3u/*/*
+
+//  &image=GFA/CLDWX
+//  &image=GFA/TURBC
+//  &image=LGF
+
+//  &image=SIG_WX//HIGH_LEVEL/*
+//  &image=SIG_WX//MID_LEVEL/*
+//  &image=SIG_WX/DEPICTION/SURFACE/*
+
+//  &image=TURBULENCE
+
+//  &image=LOW_LEVEL_WIND/FL030
+//  &image=LOW_LEVEL_WIND/FL060
+//  &image=LOW_LEVEL_WIND/FL090
+//  &image=LOW_LEVEL_WIND/FL120
+//  &image=LOW_LEVEL_WIND/FL180
+//  &image=HIGH_LEVEL_WIND/FL_240
+//  &image=HIGH_LEVEL_WIND/FL_340
+//  &image=HIGH_LEVEL_WIND/FL390
+//  &image=HIGH_LEVEL_WIND/FL450
+
+// &radius=10
+// &notam_choice=default
+// &upperwind_choice=both
+// &_=1711059916244
